5fc0195329241c8211f2a7e8a2f64e0470d8df07
Sky Luo <skluo@davidson.edu>
2025-10-20 22:53:08 -0400

    part 2.2

--- src/main/java/Combiner.java
+++ src/main/java/Combiner.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 
-/*
+/*knkd
  * This is the starter code for Part 2.2.
  * 
  * Write a single "combine" method such that all tests in CombinerTest pass,
@@ -10,56 +10,22 @@ import java.util.*;
 
 
 public class Combiner {
-	
-	public static Map<String, Set<Integer>> combine(Map<String, Integer> map1, Map<String, Integer> map2) {
 
-		List<Map<String, Integer>> maps = List.of(map1, map2);
-		
-		Map<String, Set<Integer>> result = new TreeMap<>();
-		
-		for (Map<String, Integer> map : maps) {
-			for (Map.Entry<String, Integer> entry : map.entrySet()) {
-				Set<Integer> values = result.getOrDefault(entry.getKey(), new HashSet<Integer>());
-				values.add(entry.getValue());
-				result.put(entry.getKey(), values);
-			}
-		}
-		
-		return result;
-	}
-	
-	public static Map<String, Set<String>> combine(Map<String, String> map1, Map<String, String> map2, Map<String, String> map3) {
+  public static <KeyType, ValType>  Map<KeyType, Set<ValType>> combine(Map<KeyType, ValType>... maps) {
+    if (maps == null) return Collections.EMPTY_MAP;
 
-		List<Map<String, String>> maps = List.of(map1, map2, map3);
+    HashMap<KeyType, Set<ValType>> masterMap = new HashMap<>();
 
-		Map<String, Set<String>> result = new TreeMap<>();
-		
-		for (Map<String, String> map : maps) {
-			for (Map.Entry<String, String> entry : map.entrySet()) {
-				Set<String> values = result.getOrDefault(entry.getKey(), new HashSet<String>());
-				values.add(entry.getValue());
-				result.put(entry.getKey(), values);
-			}
-		}
-		
-		return result;
-	}
-	
-	public static Map<Boolean, Set<String>> combine(Map<Boolean, String> map1, Map<Boolean, String> map2, Map<Boolean, String> map3, Map<Boolean, String> map4) {
-
-		List<Map<Boolean, String>> maps = List.of(map1, map2, map3, map4);
-
-		Map<Boolean, Set<String>> result = new TreeMap<>();
-		
-		for (Map<Boolean, String> map : maps) {
-			for (Map.Entry<Boolean, String> entry : map.entrySet()) {
-				Set<String> values = result.getOrDefault(entry.getKey(), new HashSet<String>());
-				values.add(entry.getValue());
-				result.put(entry.getKey(), values);
-			}
-		}
-		
-		return result;
-	}
+    for (Map<KeyType, ValType> m : maps) {
+      if (m == null) continue;
+      for (KeyType key : m.keySet()) {
+        ValType val = m.get(key);
+        Set<ValType> set = masterMap.getOrDefault(key, new HashSet<ValType>());
+        set.add(val);
+        masterMap.put(key, set);
+      }
+    }
+    return masterMap;
+  }
 
 }
