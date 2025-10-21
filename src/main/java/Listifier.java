a09eb22c27df341d9408fd518521d2c817363009
Sky Luo <skluo@davidson.edu>
2025-10-20 22:46:49 -0400

    part 2.1

--- src/main/java/Listifier.java
+++ src/main/java/Listifier.java
@@ -8,63 +8,16 @@ import java.util.*;
  */
 
 public class Listifier {
-	
-	public static List<Integer> listify(int a, int b, int c, int d) {
-		List<Integer> unique = new LinkedList<>();
-		unique.add(a);
-		if (unique.contains(b) == false) unique.add(b);
-		if (unique.contains(c) == false) unique.add(c);
-		if (unique.contains(d) == false) unique.add(d);
-		return unique;
-	}
-	
-	public static List<Integer> listify(int a, int b, int c, int d, int e, int f, int g) {
-		List<Integer> unique = new LinkedList<>();
-		unique.add(a);
-		if (unique.contains(b) == false) unique.add(b);
-		if (unique.contains(c) == false) unique.add(c);
-		if (unique.contains(d) == false) unique.add(d);
-		if (unique.contains(e) == false) unique.add(e);
-		if (unique.contains(f) == false) unique.add(f);
-		if (unique.contains(g) == false) unique.add(g);
-		return unique;
-	}
-	
-	
-	public static List<Double> listify(double a, double b, double c) {
-		List<Double> unique = new LinkedList<>();
-		unique.add(a);
-		if (unique.contains(b) == false) unique.add(b);
-		if (unique.contains(c) == false) unique.add(c);
-		return unique;
-	}
-	
-	public static List<Double> listify(double a, double b, double c, double d, double e) {
-		List<Double> unique = new LinkedList<>();
-		unique.add(a);
-		if (unique.contains(b) == false) unique.add(b);
-		if (unique.contains(c) == false) unique.add(c);
-		if (unique.contains(d) == false) unique.add(d);
-		if (unique.contains(e) == false) unique.add(e);
-		return unique;
-	}
-	
-	public static List<String> listify(String a, String b, String c, String d) {
-		List<String> unique = new LinkedList<>();
-		unique.add(a);
-		if (unique.contains(b) == false) unique.add(b);
-		if (unique.contains(c) == false) unique.add(c);
-		if (unique.contains(d) == false) unique.add(d);
-		return unique;
-	}
-	
-	public static List<String> listify(String a, String b, String c, String d, String e) {
-		List<String> unique = new LinkedList<>();
-		unique.add(a);
-		if (unique.contains(b) == false) unique.add(b);
-		if (unique.contains(c) == false) unique.add(c);
-		if (unique.contains(d) == false) unique.add(d);
-		if (unique.contains(e) == false) unique.add(e);
-		return unique;
-	}
+
+  public static <T> List<T> listify(T... vals) {
+      ArrayList<T> list = new ArrayList<>();
+      HashSet<T> set = new HashSet<>();
+      for (T v : vals){
+        if (!set.contains(v)){
+          list.add(v);
+          set.add(v);
+        }
+      }
+      return list;
+    }
 }
