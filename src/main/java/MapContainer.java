8a92983a7f25faa245c7200d81db829aab348847
Sky Luo <skluo@davidson.edu>
2025-10-20 22:43:18 -0400

    part 1.2

---
+++ src/main/java/MapContainer.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MapContainer<KeyType, ValType> {
+  private Map<KeyType, ValType> map;
+
+  public MapContainer() {
+    map = new HashMap();
+  }
+
+  public MapContainer(Map<KeyType, ValType> map) {
+    this.map = map;
+  }
+
+  public ValType get(KeyType key) {
+    return map.get(key);
+  }
+
+  public int size(){
+    return map.keySet().size();
+  }
+
+  public void put(KeyType key, ValType val) {
+    map.put(key, val);
+  }
+
+  public boolean put(List<KeyType> keys, List<ValType> vals) {
+    if (keys.size() != vals.size()) {
+      return false;
+    }
+
+    for (int i = 0; i < keys.size(); i++) {
+      map.put(keys.get(i), vals.get(i));
+    }
+
+    return true;
+  }
+}
